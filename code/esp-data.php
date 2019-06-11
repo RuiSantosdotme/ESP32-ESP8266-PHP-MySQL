ada5d261b47172300556d322da6963f214300bea
Rui Santos <randomnerdtutorials@gmail.com>
2019-06-11 12:05:44 +0100

    Added timezones

--- code/esp-data.php
+++ code/esp-data.php
@@ -49,7 +49,13 @@ if ($result = $conn->query($sql)) {
         $row_value1 = $row["value1"];
         $row_value2 = $row["value2"]; 
         $row_value3 = $row["value3"]; 
-        $row_reading_time = $row["reading_time"]; 
+        $row_reading_time = $row["reading_time"];
+        // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
+        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
+      
+        // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
+        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
+      
         echo '<tr> 
                 <td>' . $row_id . '</td> 
                 <td>' . $row_sensor . '</td> 
